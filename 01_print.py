e9f80c98d1a7e2d5fe7f022a18ae2cbe3c89bf62
EthanBet <ethanbet.info@gmail.com>
2025-07-26 13:11:39 +0200

    prints

--- 01_print.py
+++ 01_print.py
@@ -1,6 +1,6 @@
 ###
 # En python se puede utilizar la funcion print de la siguiente manera, tambein se le puede añadir el aprametro sep para el separador entre 
 # valores(por defecto " ") y con lo que termina (por defecto "\n").
-print("Hola mundo")
+print("Hola, mundo")
 print("1", "2", "3", "4", sep=",", end=".\n")
 
