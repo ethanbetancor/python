b8282d5bbfaad1820e09cf6f3fce56e19fe31bb4
EthanBet <ethanbet.info@gmail.com>
2025-07-26 13:10:18 +0200

    secuenciales

--- 01_print.py
+++ 01_print.py
@@ -2,5 +2,5 @@
 # En python se puede utilizar la funcion print de la siguiente manera, tambein se le puede añadir el aprametro sep para el separador entre 
 # valores(por defecto " ") y con lo que termina (por defecto "\n").
 print("Hola mundo")
-print("1", "2", "3", "4", sep=",", end=".")
+print("1", "2", "3", "4", sep=",", end=".\n")
 
